a7219243d4c12032d4900d78335d9065b132b94a
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-08-15 21:25:16 +0700

    modify url image

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
     {
         $result['sliders'] = $this->model->getHomepage('slider');
         $result['events'] = $this->model->getHomepage('event');
-        $result['src'] = url('uploads/events');
+        $result['src'] = url('uploads/events').'/';
         return view('frontend.partials.homepage', $result); 
     }
 
