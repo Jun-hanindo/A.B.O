18c5250c0522f441d25df8e87592b0842a49f933
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-08-15 21:58:36 +0700

    modify slider homepage

--- resources/views/frontend/partials/homepage.blade.php
+++ resources/views/frontend/partials/homepage.blade.php
@@ -6,9 +6,9 @@
             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                 <!-- Indicators -->
                 <ol class="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    @foreach($sliders as $key => $slider) 
+                        <li data-target="#carousel-example-generic" data-slide-to="{{ $key }}" class="{{ $key == 0 ? 'active' : ' '}}"></li>
+                    @endforeach
                 </ol>
 
 
