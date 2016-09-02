a5a784dab6de46cb876e74b559ff26ae8a3f113f
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-09-02 16:15:11 +0700

    fix system log

--- app/Models/LogActivity.php
+++ app/Models/LogActivity.php
@@ -22,7 +22,7 @@ class LogActivity extends Model
     {
         $this->user_id = $data['user_id'];
         $this->description = $data['description'];
-        $this->ip_address = '';
+        $this->ip_address = Request::ip()/*$data['ip_address']*/;
         $this->save();
 
         return $this;
