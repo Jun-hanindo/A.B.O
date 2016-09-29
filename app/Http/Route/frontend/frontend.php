2193dc9f8bbe3e74e98a718e68cdb34179391ddf
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-09-29 17:13:47 +0700

    fix route

--- app/Http/Route/frontend/frontend.php
+++ app/Http/Route/frontend/frontend.php
@@ -1,9 +1,8 @@
 <?php
 
 Route::group(['namespace' => 'Frontend'], function () {
-    //Route::get('/', array('as' => 'home', 'uses' => 'HomeController@index'));
-    Route::get('/', array('as' => 'index', 'uses' => 'HomeController@index'));
-    Route::get('home', array('as' => 'home', 'uses' => 'HomeController@home'));
+    Route::get('/', array('as' => 'home', 'uses' => 'HomeController@index'));
+    Route::get('home', array('as' => 'index', 'uses' => 'HomeController@home'));
     Route::get('bryan-adams', array('as' => 'bryan-adams', 'uses' => 'HomeController@bryanAdams'));
     Route::get('discover', array('as' => 'discover', 'uses' => 'HomeController@discover'));
     Route::get('event', array('as' => 'event', 'uses' => 'HomeController@event'));
