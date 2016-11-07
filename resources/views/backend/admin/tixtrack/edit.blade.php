709423234f98a804d288152dd79298a1581ed273
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-11-07 14:06:57 +0700

    fix flash in blade

--- resources/views/backend/admin/tixtrack/edit.blade.php
+++ resources/views/backend/admin/tixtrack/edit.blade.php
@@ -10,34 +10,33 @@
                     <h3 class="box-title">Change Account/Event to Update Data</h3>
                 </div>
                 <div class="box-body">
-                    @if(\Session::has('member'))
-                        <div class="alert alert-success alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                            {{ \Session::get('member') }}
-                        </div>
-                    @endif
-                    @if(\Session::has('transaction'))
-                        <div class="alert alert-success alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                            {{ \Session::get('transaction') }}
-                        </div>
-                    @endif
-                    @if(\Session::has('error_member'))
-                        <div class="alert alert-error alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                            {{ \Session::get('error_member') }}
-                        </div>
-                    @endif
-                    @if(\Session::has('error_transaction'))
-                        <div class="alert alert-error alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                            {{ \Session::get('error_transaction') }}
-                        </div>
-                    @endif
-                    @include('flash::message')
                     {!! Form::open(array('url' => route('admin-tixtrack-update-data'),'method'=>'PUT','id'=>'form-account')) !!}
                         <div class="box-body">
                             @include('flash::message')
+                            @if(\Session::has('member'))
+                                <div class="alert alert-success alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                    {{ \Session::get('member') }}
+                                </div>
+                            @endif
+                            @if(\Session::has('transaction'))
+                                <div class="alert alert-success alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                    {{ \Session::get('transaction') }}
+                                </div>
+                            @endif
+                            @if(\Session::has('error_member'))
+                                <div class="alert alert-error alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                    {{ \Session::get('error_member') }}
+                                </div>
+                            @endif
+                            @if(\Session::has('error_transaction'))
+                                <div class="alert alert-error alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                    {{ \Session::get('error_transaction') }}
+                                </div>
+                            @endif
                             <div id="switch-account" class="tixtrack-switch">
                                 <div class="form-group{{ Form::hasError('account') }} account">
                                     {!! Form::select('account', $account, $account_selected, array('class' => 'form-control')) !!}
