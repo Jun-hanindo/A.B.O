67e793822335951674a3119dca2ca2f7f219b442
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-11-08 13:42:16 +0700

    try to add set_time_limit when download and import

--- app/Http/Controllers/Backend/Admin/TixTrack/DownloadController.php
+++ app/Http/Controllers/Backend/Admin/TixTrack/DownloadController.php
@@ -413,7 +413,7 @@ class DownloadController extends BaseController
         }
         
         try{
-            ini_set('max_execution_time', 60);
+            ini_set('max_execution_time', 30);
             $modelCustomer = new TixtrackCustomer();
             $modelOrder = new TixtrackOrder();
             $modelAccount = new TixtrackAccount();
