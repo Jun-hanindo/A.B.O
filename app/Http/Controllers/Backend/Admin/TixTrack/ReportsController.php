5ec73ea18a2457378b7b0596dd2c1f7a99d2be7f
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-11-15 10:50:35 +0700

    fix excel image chart

--- app/Http/Controllers/Backend/Admin/TixTrack/ReportsController.php
+++ app/Http/Controllers/Backend/Admin/TixTrack/ReportsController.php
@@ -522,10 +522,10 @@ class ReportsController extends BaseController
                     $fileChartPro = 'ChartPromotion'.$event_id.'-'.$start_date.'-'.$end_date.'-'.$user.'.png';
                     $data['chartPro'] = public_path().'/uploads/charts/'.$fileChartPro;
 
-                    $drawing = new PHPExcel_Worksheet_Drawing();
-                    //$drawing->setResizeProportional();
-                    $drawing->setWidth(10);
-                    $drawing->setHeight(10);
+                    // $drawing = new PHPExcel_Worksheet_Drawing();
+                    // //$drawing->setResizeProportional();
+                    // $drawing->setWidth(10);
+                    // $drawing->setHeight(10);
 
                     $sheet->setAutoSize(false);
 
--- resources/views/backend/admin/tixtrack/export_report/excel.blade.php
+++ resources/views/backend/admin/tixtrack/export_report/excel.blade.php
@@ -119,7 +119,7 @@
             <table>
                 <tbody>
                     <tr>
-                        <td width="10px" ><img src="{{ $chartCat }}"></td>
+                        <td width="10px"><img width="10px" src="{{ $chartCat }}"></td>
                     </tr>
                 </tbody>
             </table>
@@ -216,7 +216,7 @@
             <table>
                 <tbody>
                     <tr>
-                        <td width="10px" ><img src="{{ $chartPay }}"></td>
+                        <td width="10px"><img width="10px" src="{{ $chartPay }}"></td>
                     </tr>
                 </tbody>
             </table>
@@ -326,7 +326,7 @@
             <table>
                 <tbody>
                     <tr>
-                        <td width="10px" ><img src="{{ $chartPro }}"></td>
+                        <td width="10px"><img width="10px" src="{{ $chartPro }}"></td>
                     </tr>
                 </tbody>
             </table>
