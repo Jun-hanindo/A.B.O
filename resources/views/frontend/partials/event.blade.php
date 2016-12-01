cc936eb47ff890bf3f0d91d61c309f1c7c25d04e
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-12-01 15:31:03 +0700

    fix css link promotion

--- resources/views/frontend/partials/event.blade.php
+++ resources/views/frontend/partials/event.blade.php
@@ -280,7 +280,13 @@
                                                                     </a>
                                                                 @endif
                                                                 <h3 class="font-bold">{{ $promotion->title }}</h3>
-                                                                {!! $promotion->description !!}
+                                                                    {!! $promotion->description !!}
+                                                                    @if(!empty($promotion->link_title_more_description))
+                                                                        <a id="link_title_more_promotion" data-toggle="collapse" href="#more_description" aria-expanded="false"><u> {!! $promotion->link_title_more_description !!} </u></a>
+                                                                    @endif
+                                                                    @if(!empty($promotion->more_description))
+                                                                        <span class="collapse" id="more_description"> {!! $promotion->more_description !!} </span>
+                                                                    @endif
                                                                     @if($promotion->discount > 0 || $promotion->discount_nominal > 0)
                                                                     <p>{{ trans('general.discount') }}: 
                                                                         @if($promotion->discount > 0)
@@ -304,12 +310,6 @@
                                                                 @endif
                                                                 @if(!empty($promotion->end_date))
                                                                     <p>{{ trans('frontend/general.end_date') }}: {{ full_text_date($promotion->end_date) }}</p>
-                                                                @endif
-                                                                @if(!empty($promotion->link_title_more_description))
-                                                                    <a id="link_title_more_promotion" data-toggle="collapse" href="#more_description" aria-expanded="false"><u> {!! $promotion->link_title_more_description !!} </u></a>
-                                                                @endif
-                                                                @if(!empty($promotion->more_description))
-                                                                    <span class="collapse" id="more_description"> {!! $promotion->more_description !!} </span>
                                                                 @endif
                                                             </section>
                                                         @endforeach
