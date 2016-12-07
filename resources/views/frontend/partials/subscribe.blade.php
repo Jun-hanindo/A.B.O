60cc822509b0a0087d0fb90b7f6f374978f4f2c4
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-12-07 12:46:12 +0700

    fix subscribe page

--- resources/views/frontend/partials/subscribe.blade.php
+++ resources/views/frontend/partials/subscribe.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.frontend.master.master')
-@section('title', trans('frontend/general.subscribe_us').' - ')
+@section('title', trans('frontend/general.subscribe').' - ')
 @section('og_image', asset('assets/frontend/images/logo-share.jpg'))
 @section('content')
 <div class="subscription-page">
