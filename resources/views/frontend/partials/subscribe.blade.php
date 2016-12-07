501f04703ee1820a35f7421568abb6df658c4c56
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-12-07 12:45:20 +0700

    fix subscribe page

--- resources/views/frontend/partials/subscribe.blade.php
+++ resources/views/frontend/partials/subscribe.blade.php
@@ -7,7 +7,7 @@
         <header>
             <div class="close-subscription">
                 <a href="{{ URL::previous() }}">
-                    <i class="fa fa-close"></i>{{ trans('frontend/general.cancel') }}
+                    <i class="fa fa-close"></i> {{ trans('frontend/general.cancel') }}
                 </a>
             </div>
         </header>
@@ -19,32 +19,32 @@
                             <img src="{{ asset('assets/frontend/images/subscription-logo.svg') }}" class="logo-web">
                             <img src="{{ asset('assets/frontend/images/subscribe-mobile-logo.svg') }}" class="logo-mobile">
                             <h2 class="font-light">{{ trans('frontend/general.sign_up_latest_updates') }}</h2>
-                                    <form class="form-subscribe" id="form-subscribe">
-                                        <div class="error"></div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="row">
-                                                    <div class="col-xs-6 first-name first_name">
-                                                        <input type="text" name="first_name" id="first_name" placeholder="{{ trans('frontend/general.first_name') }}" class="input-subscribe form-control">
-                                                    </div>
-                                                    <div class="col-xs-6 last-name last_name">
-                                                        <input type="text" name="last_name" id="last_name" placeholder="{{ trans('frontend/general.last_name') }}" class="input-subscribe form-control">
-                                                    </div>
+                                <form class="form-subscribe" id="form-subscribe">
+                                    <div class="error"></div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="row">
+                                                <div class="col-xs-6 first-name first_name">
+                                                    <input type="text" name="first_name" id="first_name" placeholder="{{ trans('frontend/general.first_name') }}" class="input-subscribe form-control">
                                                 </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="row">
-                                                    <div class="col-md-7 email">
-                                                        <input type="text" name="email" id="email" placeholder="{{ trans('frontend/general.email') }}" class="input-subscribe form-control">
-                                                    </div>
-                                                    <div class="col-md-5 button">
-                                                        <button type="button" class="btn btnBlackDefault font-bold" id="btnSubscribe">{{ trans('frontend/general.subscribe') }}</button>
-                                                    </div>
+                                                <div class="col-xs-6 last-name last_name">
+                                                    <input type="text" name="last_name" id="last_name" placeholder="{{ trans('frontend/general.last_name') }}" class="input-subscribe form-control">
                                                 </div>
                                             </div>
                                         </div>
-                                    </form>
-                                    <p>{{ trans('frontend/general.respect_privacy_subscription') }}</p>
+                                        <div class="col-md-6">
+                                            <div class="row">
+                                                <div class="col-md-7 email">
+                                                    <input type="text" name="email" id="email" placeholder="{{ trans('frontend/general.email') }}" class="input-subscribe form-control">
+                                                </div>
+                                                <div class="col-md-5 button">
+                                                    <button type="button" class="btn btnBlackDefault font-bold" id="btnSubscribe">{{ trans('frontend/general.subscribe') }}</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                                <p>{{ trans('frontend/general.respect_privacy_subscription') }}</p>
                                 </div>
                             </div>
                         </div>
