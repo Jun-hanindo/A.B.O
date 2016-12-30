b69ee71e2315456477cb708016f978ac1dfacc96
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-12-30 14:50:58 +0700

    fix search result page

--- resources/views/frontend/partials/search_result.blade.php
+++ resources/views/frontend/partials/search_result.blade.php
@@ -97,7 +97,7 @@
                 <div class="career-desc">
                     <div class="row">
                         <div class="job-head search-head">
-                            <h4 class="head-about head-search font-light">Search result {!! (!empty($q)) ? ' for <span>"'.$q.'"</span>' : '' !!}</h4>
+                            <h4 class="head-about head-search font-light">{!! (!empty($q)) ? trans('frontend/general.search_result_for').' <span>"'.$q.'"</span>' : 'Search results' !!}</h4>
                             {{-- <select id="sort-search" name="sort" class="form-control">
                                 <option value="date" {{ $sort == 'date' ? 'selected' : '' }}>Sort By Date</option>
                                 <option value="price" {{ $sort == 'price' ? 'selected' : '' }}>Sort By Price</option>
