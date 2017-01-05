0343257a2fdfebf401cdb60d3b6d8140de6ec45d
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2017-01-05 13:32:31 +0700

    fix venue master, add html 5

--- resources/views/backend/admin/venue/script/index_script.blade.php
+++ resources/views/backend/admin/venue/script/index_script.blade.php
@@ -5,6 +5,7 @@
     <script>
     $(document).ready(function() {
         loadData();
+        //loadSwitchButton('avaibility-check');
         
         // $('.select_all-checkbox').on('click', function(){
         //     datatablesSelectAll(loadData());
@@ -14,24 +15,24 @@
         //     datatablesCheckbox(loadData());
         // });
 
-        // $('#venue-datatables').on('switchChange.bootstrapSwitch', '.avaibility-check', function(event, state) {
-        //     var id = $(this).attr('data-id');
-        //     var uri = "{{ URL::route('admin-update-venue-avaibility', "::param") }}";
-        //     uri = uri.replace('::param', id);
-        //     var val = $(this).is(":checked") ? true : false;
-        //     $.ajax({
-        //             url: uri,
-        //             type: "POST",
-        //             dataType: 'json',
-        //             data: "avaibility="+val,
-        //             success: function (data) {
-        //                 $('.error').html('<div class="alert alert-success">' + data.message + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>');
-        //             },
-        //             error: function(response){
-        //                 $('.error').html('<div class="alert alert-danger">' + response.responseJSON.message + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>');
-        //             }
-        //         });
-        // });
+        $('#venue-datatables').on('switchChange.bootstrapSwitch', '.avaibility-check', function(event, state) {
+            var id = $(this).attr('data-id');
+            var uri = "{{ URL::route('admin-update-venue-avaibility', "::param") }}";
+            uri = uri.replace('::param', id);
+            var val = $(this).is(":checked") ? true : false;
+            $.ajax({
+                    url: uri,
+                    type: "POST",
+                    dataType: 'json',
+                    data: "avaibility="+val,
+                    success: function (data) {
+                        $('.error').html('<div class="alert alert-success">' + data.message + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>');
+                    },
+                    error: function(response){
+                        $('.error').html('<div class="alert alert-danger">' + response.responseJSON.message + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>');
+                    }
+                });
+        });
 
     });
 
