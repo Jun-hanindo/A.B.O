48e6ccc16b2614d0d8bc5dfd1a6f80e6aabd4244
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2017-01-12 12:09:56 +0700

    fix css, try session

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 1,
+    'lifetime' => 2,
 
-    'expire_on_close' => true,
+    'expire_on_close' => false,
 
     /*
     |--------------------------------------------------------------------------
